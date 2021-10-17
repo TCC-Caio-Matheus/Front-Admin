a4dd9a2df745bbcdc9f3a22f17961c9b0eb8717a
Caio <caiooliv97@gmail.com>
2021-10-17 18:27:13 -0300

    merge

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    green: '#8ede63',
+    greenLight: '#CDEEBF',
+    GREEN_LIGHTER: '#f0fbea',
+    greenDark: '#52b232',
+    greenDarker: '#135434',
+    GRAY_LIGHT: '#f0f0f0',
+    GRAY: '#cfcfcf',
+    BLACK: '#252733',
+    RED: '#D34407',
+    RED_LIGHT: '#E9B09A',
+    RED_LIGHTER: '#FAECE6',
+    YELLOW_LIGHT: '#f5ffb6',
+  },
+};
+
+export default theme;
